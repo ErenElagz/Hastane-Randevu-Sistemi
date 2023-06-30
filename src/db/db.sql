16cafa000b69ba5ef04ac9da26ade1ef5039c2b8
ErenElagz <erenelagoz@gmail.com>
2023-06-30 18:14:23 +0300

    Update

--- src/db/db.sql
+++ src/db/db.sql
@@ -5,15 +5,15 @@ CREATE TABLE randevu (
     name varchar(255),
     surname varchar(255),
     tc varchar(255),
-    city varchar(255)
-    department varchar(255)
+    city varchar(255),
+    department varchar(255),
     date varchar(255)
 );
 
 CREATE TABLE kullanıcılar (
     id int PRIMARY AUTO_INCREMENT,
     name varchar(255),
-    password varchar(255),
+    password varchar(255)
 
 );
 
